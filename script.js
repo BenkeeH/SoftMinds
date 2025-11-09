ecda48c85927cd91be63f1d4a8ebcd85cec740db
BenkeeH <benkehunorwork@gmail.com>
2025-11-09 20:39:19 +0100

    slider kep kesz kepeket kell majd kesziteni

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ faqs.forEach(btn => {
     faq.classList.toggle('open');
   });
 });
+
+
+const heroImages = [
+  "./img/slide1.jpg",
+  "./img/slide2.jpg",
+  "./img/slide3.jpg"
+];
+
+let currentHeroIndex = 0;
+const heroImgEl = document.getElementById("hero-img");
+const nextBtn = document.getElementById("next-btn");
+
+nextBtn.addEventListener("click", () => {
+  currentHeroIndex = (currentHeroIndex + 1) % heroImages.length;
+  heroImgEl.src = heroImages[currentHeroIndex];
+});
